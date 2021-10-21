f4f608eeea921592df5040bbeb59e19bb87791cd
JD760 <jacobdicken2005@gmail.com>
2021-10-21 21:52:43 +0000

    Developing threadpool system

--- src/pool.cpp
+++ src/pool.cpp
@@ -3,27 +3,59 @@
 #include <iostream>
 #include <thread>
 
+
+/* 
+ * Create the threadpool
+ * Creates a new thread in which to execute the threadpool else
+ * the ability to control the pool would be lost 
+ * when started, pool waits for tasks and a subsequent start execution
+ * signal before beginning task processing
+*/
+threadpool::pool::pool(int numThreads) {  // constructor
+    this->poolState = Sleeping;
+    
+    /* Set up mutexes */
+    pthread_mutex_init(&this->queueLock, NULL);
+    pthread_mutex_init(&this->returnLock, NULL);
+
+    /* set up task queue */
+    this->queueEnd = this->queueFront = NULL;
+    this->queueLength = 0;
+
+    /* set up conditions */
+    pthread_cond_init(&this->newTasks, NULL);
+
+    /* create the threads and start worker functions in sleep state */
+    this->threads = (pthread_t *) std::malloc(sizeof(pthread_t) * numThreads);
+    for (int i = 0; i < numThreads; i++) {
+        pthread_create(
+                &this->threads[i], NULL, threadpool::workerThread, NULL
+        );
+        
+    }
+    return;
+}
+
+ int threadpool::startPool() {
+     return 0;
+ }
+
+void threadpool::pool::sleep() {
+    this->poolState = Sleeping;
+};
+
 int threadpool::pool::addTask( threadpool::task *task) {
     if (this->queueLength != 0) {
         task->next = this->queueEnd;
         task->prev = NULL;
         this->queueEnd = task;
-        this->queueLength++;
+        task->id = task->prev->id + 1;
     } else {
         queueEnd = queueFront = task;
-        this->queueLength++;
+        task->id = 1;
     }
     this->queueLength++;
-
-    return 0;
-}
-
-int threadpool::pool::shutdown(int mode) {
-    if (mode == 3) {
-    this->poolState = Shutdown;
-    } 
-    else if (mode == 4) {
-        this->poolState = ForcefulShutdown;
-    }
+    
+    std::cout << this->queueLength;
     return 0;
 }
